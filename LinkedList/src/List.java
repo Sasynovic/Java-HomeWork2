9c6759d55bac2172436bed0e4ceef7e579e5f4e3
Salvatore Barretta <barrettasalvatore@outlook.it>
2024-07-06 13:35:00 +0200

    firsti commit : trying to implement manually the linkedlist. Firs try

---
+++ LinkedList/src/List.java
@@ -0,0 +1,28 @@
+public class List {
+
+    Node head;
+
+    public void insert(int data) {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+
+    public void show() {
+        Node node = head;
+        do {
+            System.out.println(node.data);
+            node = node.next;
+        }while (node != null);
+    }
+}
